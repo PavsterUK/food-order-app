3401fdcc35acf9b6a562b3a257beef36fac356da
PavsterUK <pavelnaumovic@gmail.com>
2022-01-20 22:08:36 +0000

    main page

--- src/HomePage/HomePage.js
+++ src/HomePage/HomePage.js
@@ -1,13 +1,16 @@
-import React from 'react'
+import React from "react";
 
-import Header from "../HomePage/Header"
+import Header from "../HomePage/Header";
+import styles from "./HomePage.module.css";
+
 
 const HomePage = () => {
   return (
-    <div>
-      <Header/>
+    <div className={styles.homepage}>
+      <Header />
+      <div className={styles.bg_img}></div>
     </div>
-  )
-}
+  );
+};
 
-export default HomePage
+export default HomePage;
