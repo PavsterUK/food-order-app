a03607d79601f811a1f283cecc1668598391b72b
PavsterUK <pavelnaumovic@gmail.com>
2022-02-01 22:08:41 +0000

    cart total items

--- src/contexts/ItemsToOrderContext.js
+++ src/contexts/ItemsToOrderContext.js
@@ -1,9 +1,6 @@
 import React from 'react';
 
-export const ItemsToOrderContext = React.createContext({
-  itemsToOrder: [],
-  setitemsToOrder: () => {}
-});
+export const ItemsToOrderContext = React.createContext();
 
 
 
